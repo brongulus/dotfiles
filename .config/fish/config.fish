89a4a2170644b784d6dc1a666f897dd292964a9f
brongulus <prashantrameshtak@gmail.com>
2022-10-22 11:15:35 +0530

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,14 @@ if status is-interactive
     if [ -f $HOME/.config/alias ]
     source $HOME/.config/alias
     end
+    
+    set PATH ~/.emacs.d/bin $PATH
+    set PATH ~/bin $PATH
+    set PATH ~/.cargo/bin $PATH
+
+    function f
+        fff $argv
+        set -q XDG_CACHE_HOME; or set XDG_CACHE_HOME $HOME/.cache
+        cd (cat $XDG_CACHE_HOME/fff/.fff_d)
+    end
 end
